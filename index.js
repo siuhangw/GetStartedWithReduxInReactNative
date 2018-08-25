3dd6c61efea3d18e9c0f727da6be3c1a9bbc9208
siuhangw <wshiahb@gmail.com>
2018-08-25 18:17:18 +0800

    React native navigation install success

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 /** @format */
 
-import { AppRegistry } from "react-native";
+// import { AppRegistry } from "react-native";
 import App from "./App";
 
-import { name as appName } from "./app.json";
+// import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+import { Navigation } from "react-native-navigation";
+
+Navigation.registerComponent("example.FirstTabScreen", () => App);
+Navigation.registerComponent("example.SecondTabScreen", () => App);
+
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: "One",
+      screen: "example.FirstTabScreen", // this is a registered name for a screen
+      //   icon: require("./img/logo.png"),
+      title: "Screen One"
+    },
+    {
+      label: "Two",
+      screen: "example.SecondTabScreen",
+      //   icon: require("./img/logo.png"),
+      title: "Screen Two"
+    }
+  ]
+});
